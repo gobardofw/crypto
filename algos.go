a8b9d91bf5f2a757c7875d08cfda1f74a77a4415
M Ekramy <m.ajamekramy@gmail.com>
2020-10-17 19:12:20 +0330

    fix parse method

--- algos.go
+++ algos.go
@@ -8,41 +8,41 @@ import (
 type HashAlgo int
 
 // Parse algo from string
-func (a HashAlgo) Parse(algo string) bool {
+func (a *HashAlgo) Parse(algo string) bool {
 	algo = strings.ToUpper(algo)
 	switch algo {
 	case "MD4":
-		a = MD4
+		*a = MD4
 	case "MD5":
-		a = MD5
+		*a = MD5
 	case "SHA1":
-		a = MD5
+		*a = MD5
 	case "SHA256":
-		a = SHA256
+		*a = SHA256
 	case "SHA256224":
-		a = SHA256224
+		*a = SHA256224
 	case "SHA512":
-		a = SHA512
+		*a = SHA512
 	case "SHA512224":
-		a = SHA512224
+		*a = SHA512224
 	case "SHA512256":
-		a = SHA512256
+		*a = SHA512256
 	case "SHA384":
-		a = SHA384
+		*a = SHA384
 	case "SHA3224":
-		a = SHA3224
+		*a = SHA3224
 	case "SHA3256":
-		a = SHA3256
+		*a = SHA3256
 	case "SHA3384":
-		a = SHA3384
+		*a = SHA3384
 	case "SHA3512":
-		a = SHA3512
+		*a = SHA3512
 	case "KECCAK256":
-		a = KECCAK256
+		*a = KECCAK256
 	case "KECCAK512":
-		a = KECCAK512
+		*a = KECCAK512
 	default:
-		a = 0
+		*a = 0
 		return false
 	}
 	return true
